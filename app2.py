3427c419e54e141b1e227639c695de78eca0047d
musicteaice <musicteaice@gmail.com>
2026-04-28 04:51:21 +0800

    解决字体问题

--- app2.py
+++ app2.py
@@ -6,7 +6,7 @@ import streamlit as st
 import io
 from datetime import datetime
 
-plt.rcParams['font.sans-serif'] = ['DejaVu Sans'] 
+plt.rcParams["font.sans-serif"] = "Noto Sans CJK JP"
 plt.rcParams['axes.unicode_minus'] = False
 
 class Interferometer3x3Demodulator:
